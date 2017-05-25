a056727ed7e62f1dc58da30496f4d80e92cd16f2
Jonathan Weth <jonathanweth@Ruth-Harders-iMac.local>
2017-05-25 15:40:05 +0200

    Finish migration to OOP | Add docstrings

--- wuerfel.py
+++ wuerfel.py
@@ -1,108 +1,57 @@
-
 import time as t
+from wuerfellib import Wuerfel, Timer, Statistik
 
 
-class Wuerfel(object):
-	import random as rd
-
-	def __init__(self):
-		pass
-
-	def wuerfele():
-		return self.rd.randinit(1,6)
-
-class Statistik(object):
-	import csv as csv
-	Statistik = {}
-
-	def __init__(self, keys = "123456", keytitle = "Wert", valuetitle = "Anzahl"]):
-		# Initialisiere Statistik-Array
-		for key in keys:
-			self.Statistik[key] = 0
-		# Titel festlegen
-		self.KeyTitle = keytitle
-		self.ValueTitle = valuetitle
-
-	def writecsv(self, filename, extrainfos={"Test": "Test"}):
-		with open(filename, 'wt') as csvfile:
-			csvwriter = csv.writer(csvfile)
-			# Titelzeile schreiben
-			csvwriter.writerow([self.KeyTitle, self.ValueTitle])
-			# Schreibe Statistik
-			for key in self.Statistik.keys():
-  				csvwriter.writerow([key, str(self.Statistik[key])])
-			# Schreibe Zusatzinfo
-			for key in extrainfos.keys():
-				csvwriter.writerow([key, extrainfos[key]])
-
-	def add(self, key):
-		self.Statistik[key] = self.Statistik[key] + 1
-
-class Timer(object):
-	import time as t
-	Time = 0
-	StartTime = 0
-	EndTime = 0
-
-	def __init__(self):
-		pass
-
-	def start(self):
-		if(self.Running == False):
-			self.StartTime = t.time()
-			self.Running = True
-			self.Time = 0
-		else:
-			print("Bitte stoppe zuerst den Timer!")
-
-	def stop(self):
-		if(self.Running):
-			self.EndTime = t.time()
-			self.Running = False
-			self.Time = self.EndTime - self.StartTime
-		else:
-			print("Bitte starte zuerst den Timer.")
-
-	def get(self):
-		return self.Time
-		
 while True:
-	try:
-		statistik = Statistik("123456", "Augenzahl", "Anzahl")
+	# Initialisiere Objekte
+	statistik = Statistik("123456", "Augenzahl", "Anzahl")
+	wuerfel = Wuerfel()
+	timer = Timer()
+
+	# Frage Anzahl der Würfe ab
+	while True:
 		menge = input ("Wie oft soll gewürfelt werden? ")
-		menge = int(menge)
-		starttime = t.time()
+		if(menge.isnumeric):
+			menge = int(menge)
+			break
+		else:
+			print("Bitte gebe eine gültige Zahl ein")
 
-		# Würfeln
-		for i in range(menge):
-			a = wuerfele()
-			# b = wuerfele()
-			c = a # + b
-			#print ("Würfel 1: "+str(a))
-			#print ("Würfel 2: "+str(b))
-			#print ("Würfelaugensumme: "+str(c))
-			#print ("-----")
-			statistik[c] = statistik[c] + 1
+	# Starte den Timer
+	timer.start()
 
-		# Gebe Statistik aus
-		endtime = t.time()
-		print ("#############")
-		print ("# Statistik #")
-		print ("#############")
-		# Würfel 0-12
-		for i in range(len(statistik)):
-			print(str(i) + "er: " + str(statistik[i]))
-		print()
-		time = endtime - starttime
-		print("Dauer in Sekunden für " + str(menge) + "x Würfeln: " + str(time))
+	# Würfeln
+	for i in range(menge):
+		x = wuerfel.wuerfele()
+		statistik.add(str(x))
 
-		# Schreibe Statistik in csv
-		statfile = "wuerfeln" + str(endtime) + ".csv"
-		writestat(statistik, statfile, menge)
-		print("#############")
-		print("Ergebnisse in csv: " + statfile)
+	# Stoppe Timer
+	timer.stop()
+
+	# Gebe Statistik aus
+	statistik.show()
+
+	# Gebe gebrauchte Zeit aus
+	print("--")
+	print("Dauer in Sekunden für " + str(menge) + "x Würfeln: " + str(timer.Time))
+
+	# Schreibe Statistik in csv
+	try:
+		statfile = "wuerfeln" + str(timer.Time) + ".csv"
+		print("--")
+		print("CSV-Datei mit Ergebnissen: " + statfile)
+		statistik.writecsv(statfile, {"Anzahl der Würfe: ": menge})
 	except:
-		print("FEHLER!")
-		exit()
+		print("Leider konnte die CSV-Datei nicht geschrieben werden!")
 
+	print("######################################")
+	print("######################################")
+
+	# Lösche die Objekte wieder
+	del menge, statfile
+	del wuerfel
+	del statistik
+	del timer
+
+	# Mache 1 Sekunde Pause
 	t.sleep(1)
---
+++ wuerfellib.py
@@ -0,0 +1,116 @@
+class Wuerfel(object):
+	import random as rd
+
+	def __init__(self):
+		pass
+
+	def wuerfele(self):
+		""" Würfelt eine Zahl zwischen 1 und 6
+
+		Argumente: 	keine
+		Rückgabe: 	gewürfelte Zahl
+		"""
+
+		return self.rd.randint(1,6)
+
+class Statistik(object):
+	import csv as csv
+	Statistik = {}
+
+	def __init__(self, keys = "123456", keytitle = "Wert", valuetitle = "Anzahl"):
+		""" Initialisiert ein neues Statistikobjekt
+
+		Argumente:	keys > Array oder String > Schlüsselwörter für die statistischen Objekte > Standard: 123456
+					keytitle > String > Titel für die Schlüsselwörter > Standard: Wert
+					valuetitle > String > Titel für die erfassten Werte > Standard: Anzahl
+		Rückgabe: 	keine
+		"""
+
+		# Initialisiere Statistik-Array
+		for key in keys:
+			self.Statistik[key] = 0
+		# Titel festlegen
+		self.KeyTitle = keytitle
+		self.ValueTitle = valuetitle
+
+	def writecsv(self, filename = "ergebnisse.csv", extrainfos = {"": ""}):
+		""" Schreibt die Statistik in eine CSV-Datei
+
+		Argumente: 	filename > String > Name der CSV-Datei
+					extrainfos > Dictionary > zusätzliche Infos, die auch in die CSV-Datei geschrieben werden
+		Rückgabe: 	keine
+		"""
+
+		with open(filename, 'wt') as csvfile:
+			csvwriter = self.csv.writer(csvfile)
+			# Titelzeile schreiben
+			csvwriter.writerow([self.KeyTitle, self.ValueTitle])
+			# Schreibe Statistik
+			for key in self.Statistik.keys():
+  				csvwriter.writerow([key, str(self.Statistik[key])])
+			# Schreibe Zusatzinfo
+			for key in extrainfos.keys():
+				csvwriter.writerow([key, extrainfos[key]])
+
+	def show(self):
+		""" Zeigt die Statistik im CLI
+
+		Argumente:	keine
+		Rückgabe:	keine
+		"""
+
+		print("#############")
+		print("# Statistik #")
+		print("#############")
+		for key in self.Statistik.keys():
+			print(key + ": " + str(self.Statistik[key]))
+
+	def add(self, key):
+		self.Statistik[key] = self.Statistik[key] + 1
+
+class Timer(object):
+	import time as t
+	Time = 0 # gemessene Zeit
+	StartTime = 0 # Start der Messung
+	EndTime = 0 # Ende der Messung
+	Running = False # Läuft die Messung?
+
+	def __init__(self):
+		pass
+
+	def start(self):
+		""" Startet den Timer
+
+		Argumente: 	keine
+		Rückgabe: 	keine
+		"""
+
+		if(self.Running == False):
+			self.StartTime = self.t.time()
+			self.Running = True
+			self.Time = 0
+		else:
+			print("Bitte stoppe zuerst den Timer!")
+
+	def stop(self):
+		""" Stoppt den Timer
+
+		Argumente: 	keine
+		Rückgabe: 	keine
+		"""
+
+		if(self.Running):
+			self.EndTime = self.t.time()
+			self.Running = False
+			self.Time = self.EndTime - self.StartTime
+		else:
+			print("Bitte starte zuerst den Timer.")
+
+	def get(self):
+		""" Übergibt die gemessene Zeit
+
+		Argumente:	keine
+		Rückgabe: 	gemessene Zeit
+		"""
+
+		return self.Time
